7362ac266da8a4f82b04f37c171e9d71665e926e
root <root@node.monocluster.com>
2019-04-21 23:43:38 +0530

    updated test class

--- zipkin-server/src/test/java/com/example/zipkinsever/ZipkinServerApplicationTests.java
+++ zipkin-server/src/test/java/com/example/zipkinsever/ZipkinServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZipkinService1ApplicationTests {
+public class ZipkinServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
